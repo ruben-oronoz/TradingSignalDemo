e86504ecdaf133c920e0bf5d39ba65b0e54fb1a7
Ruben_Python <oronozruben37@gmail.com>
2026-03-13 02:53:48 -0400

    Create MainActivity.kt

---
+++ app/src/main/java/com/tradesignal/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.tradesignal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+
+    Text("TradeSignal Demo Running")
+}
